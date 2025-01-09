33577776b0f5f38508b3d87ec1b3626744ffdb17
Kai Teoh <kai.teoh@gmail.com>
2025-01-09 08:45:04 -0800

    keep only latest records

--- test.py
+++ test.py
@@ -2,9 +2,13 @@ import pandas as pd
 
 file_path = 'Storm_Individual_Power_Outages.csv'
 data = pd.read_csv(file_path)
+print(data.shape)
+data['start_time_local'] = pd.to_datetime(data['start_time_local'], format='mixed')
 
+latest_records = data.loc[data.groupby('sensor_id')['start_time_local'].idxmax()]
+print(latest_records.shape)
 outage_time = "2025-12-31T23:59:59-06:00"
-sensors_with_specific_outage = data[data['end_time_local'] == outage_time]
+sensors_with_specific_outage = latest_records[latest_records['end_time_local'] == outage_time]
 
 sensor_list = sensors_with_specific_outage['sensor_id'].unique().tolist()
 
