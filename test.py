664be8ea030518687e9e1a72bb6d90669e657f52
Kai Teoh <kai.teoh@gmail.com>
2025-01-09 07:00:20 -0800

    Add dallas data

--- test.py
+++ test.py
@@ -3,14 +3,11 @@ import pandas as pd
 file_path = 'Storm_Individual_Power_Outages.csv'
 data = pd.read_csv(file_path)
 
-# # Filter sensors where the outage end_time_local matches the specific date
 outage_time = "2025-12-31T19:59:59-08:00"
 sensors_with_specific_outage = data[data['end_time_local'] == outage_time]
 
-# # Extract the list of sensor IDs
 sensor_list = sensors_with_specific_outage['sensor_id'].unique().tolist()
 
-# # Output the sensor list
 print(len(sensor_list))
 
 og_file = 'Sensor_Location.csv'
